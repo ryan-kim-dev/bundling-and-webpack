56fa4462706f7549f01184b3f160bb32af213891
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-07-25 14:07:46 +0900

    advanced challange

--- vanila-todo-app/webpack.config.js
+++ vanila-todo-app/webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const isDevMode = process.env.NODE_ENV.includes('dev');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-exact-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // './dist'의 절대 경로를 리턴합니다.
+    path: path.resolve(__dirname, 'docs'), // './dist'의 절대 경로를 리턴합니다.
     filename: 'app.bundle.js',
+    clean: true,
   },
   module: {
     rules: [
@@ -15,7 +20,11 @@ module.exports = {
         // 배열 마지막 요소부터 오른쪽에서 왼쪽 순으로 적용
         // 먼저 css-loader가 적용되고, styled-loader가 적용되어야 한다.
         // 순서 주의!
-        use: ['style-loader', 'css-loader'],
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
         // loader가 node_modules 안의 있는 내용도 처리하기 때문에
         // node_modules는 제외해야 합니다
         exclude: /node_modules/,
@@ -26,5 +35,21 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
   ],
+  //   mode: 'development',
+  optimization: {
+    // 압축
+    minimize: isDevMode ? false : true,
+    // 미니마이저
+    minimizer: [
+      // 플러그인 인스턴스 생성
+      new CssMinimizerPlugin({
+        // CPU 멀티 프로세서 병렬화 옵션 (기본 값: true)
+        parallel: os.cpus().length - 1,
+      }),
+    ],
+  },
 };
